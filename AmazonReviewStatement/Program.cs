e6c51b87bc3d475e18608babd7c5d7b0143a29b4
Ben360xy <ben.r.victor@gmail.com>
2024-11-01 10:52:34 -0400

    Project: Ai Model working with a 75% rate

--- AmazonReviewStatement/Program.cs
+++ AmazonReviewStatement/Program.cs
@@ -1,4 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AmazonReviewStatement;
 
 
+
+
+//Load sample data
+var sampleData = new DumbAi.ModelInput()
+{
+    Review_text = @"Wow what a great game!",
+
+};
+
+//Load model and predict output
+var result = DumbAi.Predict(sampleData);
+
+Console.WriteLine("1 = Negative Review\n2 = Positive Review");
+
+Console.WriteLine(result.PredictedLabel + " is the review score");
+
+
+if (result.PredictedLabel == 1)
+{
+    Console.WriteLine("The Review is Negative :(");
+}
+else
+{
+    Console.WriteLine("The Review is Positive! :)");
+}
